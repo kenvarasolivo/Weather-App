12a8aff10174af02aaeef0c194a2f80dd95d5ba2
kenvara <kenvara.lwie@alumni.fh-aachen.de>
2025-06-25 17:22:45 +0200

    updated

---
+++ script.js
@@ -0,0 +1,59 @@
+const cityInput = document.querySelector('.city-input')
+const searchBtn = document.querySelector('.search-btn')
+
+const notFoundSection = document.querySelector('.not-found')
+const SearchCitySection = document.querySelector('.search-city')
+const weatherInfoSection =  document.querySelector('.weather-info')
+
+
+
+const apiKey = '94f81ee2247c07f061c1fbda7d744413'
+
+searchBtn.addEventListener('click', (event) =>{
+    if (cityInput.value.trim() != ''){
+        updateWeatherInfo(cityInput.value)
+        cityInput.value=''
+        cityInput.blur()
+    }
+})
+
+cityInput.addEventListener('keydown', (event)=> {
+    if (event.key == "Enter" && cityInput.value.trim() != ''){
+        updateWeatherInfo(cityInput.value)
+        cityInput.value=''
+        cityInput.blur()
+    }
+})
+
+async function getFetchData(endPoint, city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
+    const response = await fetch(apiUrl)
+
+    return response.json()
+}
+
+async function updateWeatherInfo(city){
+    const weatherData = await getFetchData('weather', city)
+
+    if(weatherData.cod != 200){
+        showDisplaySection(notFoundSection)
+        return
+    }
+    console.log(weatherData)
+
+    /* const {
+        name: country,
+        main: [{ temp, humidity }],
+        weather: [{ id, main }],
+        wind: {speed}
+    } = weatherData */
+
+    showDisplaySection(weatherInfoSection)
+}
+
+function showDisplaySection(section) {
+    [weatherInfoSection, SearchCitySection, notFoundSection]
+        .forEach(section => section.style.display = 'none')
+
+    section.style.display='flex'
+}
